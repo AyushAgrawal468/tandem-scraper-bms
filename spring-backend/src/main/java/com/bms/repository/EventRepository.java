42b52f435cf207be10a72c88a652e1526892665e
AyushAgrawal468 <ayushagrawal468@gmail.com>
2025-10-23 02:11:00 +0530

    spring backend deleted

--- spring-backend/src/main/java/com/bms/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bms.repository;
-
-import com.bms.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
